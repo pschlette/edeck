78b9b7219fb61b6deb8466c0ea0ecece4a30680a
Pete Schlette <pete.schlette@gmail.com>
2016-12-14 07:57:02 +0000

    Add a few deck-related routes and generate deck IDs

--- src/main/scala/com/github/pschlette/edeck/EdeckServlet.scala
+++ src/main/scala/com/github/pschlette/edeck/EdeckServlet.scala
@@ -1,5 +1,6 @@
 package com.github.pschlette.edeck
 
+import java.util.UUID
 import org.scalatra._
 
 class EdeckServlet extends EdeckStack {
@@ -8,9 +9,22 @@ class EdeckServlet extends EdeckStack {
     <html>
       <body>
         <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <form method="POST" action="decks">
+          <p>
+            This is the e-dom editor for collaborative kingdoms.<br/>
+            Click the button to generate a new deck: <input type="submit"/>
+          </p>
+        </form>
       </body>
     </html>
   }
 
+  post("/decks") {
+    val newDeckId = UUID.randomUUID.toString
+    <p>You posted to /decks. Your new deck id would be {newDeckId} if that was a thing yet.</p>
+  }
+
+  get("/decks/:id") {
+    <p>You're viewing the deck with id {params("id")} - or, you WOULD be, if that was a thing.</p>
+  }
 }
