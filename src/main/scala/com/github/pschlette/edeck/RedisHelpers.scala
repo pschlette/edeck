ab048818e0ff0b725fd8e47287aae7c168eda853
Pete Schlette <pete.schlette@gmail.com>
2016-12-16 06:11:36 +0000

    Add a few more key helpers

--- src/main/scala/com/github/pschlette/edeck/RedisHelpers.scala
+++ src/main/scala/com/github/pschlette/edeck/RedisHelpers.scala
@@ -4,6 +4,10 @@ object RedisHelpers {
   private def withNamespace(keyName: String) = s"edeck:${keyName}"
   private def createDeckKey(keyName: String, deckId: String) = withNamespace(s"${keyName}:${deckId}")
 
+
   val KingdomCardsKey = withNamespace("kingdomCards")
+
   def deckTimestampKey(deckId: String) = createDeckKey("creationTime", deckId)
+  def deckProposedCardsKey(deckId: String) = createDeckKey("proposedCards", deckId)
+  def deckHistoryKey(deckId: String) = createDeckKey("history", deckId)
 }
